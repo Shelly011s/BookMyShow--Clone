d981012e3497fcc003a3f627c0367cd2e0a2fc07
Shelly Suthar <84381373+Shelly011s@users.noreply.github.com>
2022-01-11 23:40:06 +0530

    Update Cast.component.js

--- src/components/MovieHero/Cast.component.js
+++ src/components/MovieHero/Cast.component.js
@@ -2,14 +2,6 @@ import React from 'react';
 
 const Cast = (props) => {
   return (
-<<<<<<< HEAD
-    <div className='flex flex-col justify-center'>
-      <img
-        className=' px-3 rounded-full h-20 w-25'
-        src={props.src}
-        alt='cast'
-      />
-=======
     <div className='flex flex-col items-center'>
       <div className="w-20 h-20">
           <img
@@ -18,7 +10,6 @@ const Cast = (props) => {
             className="w-full h-full rounded-full"
           />
         </div>
->>>>>>> master
       <h4 className='mx-auto mt-2'>{props.name}</h4>
       <h4 className=' mx-auto text-gray-500'>{props.castName}</h4>
     </div>
